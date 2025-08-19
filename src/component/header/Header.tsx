92a10278bd67c0ac65794c6de1f6b880c1e18ca7
Abhishek-Seek <abhishek0101811@gmail.com>
2025-08-19 11:09:52 +0530

    done

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -309,7 +309,7 @@ const Header = () => {
   // );
 
   const MegaMenuContent = () => (
-    <div className="bg-blue-900 text-white grid grid-cols-3 lg:grid-cols-4 gap-4 xl:gap-8 p-14">
+    <div className="bg-blue-900 text-white grid grid-cols-3 xl:grid-cols-4 gap-6 xl:gap-8 p-14">
       <div className="text-sm font-bold flex flex-col gap-3 font-serif">
         <a
           href="https://mabsolinfotech.com/crmtest/employee/employee_register.php"
