e472702bb3d101abfcc2f44973944eaa4dcc3a09
careerofshweta <shweta5662.sav@gmail.com>
2025-11-19 17:35:28 +0530

    toast message done bhul gaye the save krna

--- src/components/google-review/GoogleReview.tsx
+++ src/components/google-review/GoogleReview.tsx
@@ -44,7 +44,7 @@ const GoogleReview = () => {
     return (
         <div className='lg:px-20 px-6 py-6 bg-white'>
             <script src="https://elfsightcdn.com/platform.js" async></script>
-<div className ="elfsight-app-c5fe587d-7bbf-4c08-aaa3-cfd569414b2a" data-elfsight-app-lazy></div>
+            <div className="elfsight-app-c5fe587d-7bbf-4c08-aaa3-cfd569414b2a" data-elfsight-app-lazy></div>
 
 
         </div>
